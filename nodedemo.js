50988c71a88d65252e0513060ed54e6b80114a39
Jonathan Hinds <jonathanhinds@zipcoders-MacBook-Pro-90.local>
2018-11-28 16:29:24 -0500

    trying to allow cors

--- nodedemo.js
+++ nodedemo.js
@@ -9,8 +9,10 @@ const port = process.env.PORT;
 //create a new server using the http module
 const server = http.createServer((req, res) => 
 { 
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   //here are some changes
   var content;
   //set the body to be an array
